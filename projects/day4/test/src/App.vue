a953969b96b63cdddc52a43fcb8ed081dc3177e3
wangyong16 <wangyong16@xiaomi.com>
2023-03-19 07:02:37 +0800

    feat: 手动按需引入

---
+++ projects/day4/test/src/App.vue
@@ -0,0 +1,49 @@
+<script>
+import PButton from "p-ui/lib/button";
+import PHeader from "p-ui/lib/header";
+import "p-ui/lib/styles/button.css";
+import "p-ui/lib/styles/header.css";
+export default {
+  name: "APP",
+  components: {
+    PButton,
+    PHeader,
+  },
+};
+</script>
+
+<template>
+  <div>
+    <p-header>头部组件</p-header>
+    <p-button type="primary">我是按钮</p-button>
+  </div>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ projects/day4/test/src/main.js
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+// import PUI from 'p-ui';
+// import 'p-ui/lib/styles/index.css'
+// console.log(`PUI`, PUI);
+
+createApp(App)
+// .use(PUI)
+.mount('#app')
